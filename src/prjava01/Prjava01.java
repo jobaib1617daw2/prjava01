a59ec16491253b9e65520bfe32bd214a1c2324f2
joanBaro <jbaro.clot@gmail.com>
2017-01-18 21:19:27 +0100

    Versió 0.1 del projecte prjava01

---
+++ src/prjava01/Prjava01.java
@@ -0,0 +1,41 @@
+/**
+ *
+ * @author XYZ1617daw2
+ */
+
+package prjava01;
+import java.io.IOException;
+import java.io.*;
+
+public class Prjava01 {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        File f = new File("fitxer.html");
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(f))) {
+            bw.write("<html>");
+            bw.newLine();
+            bw.write("  <head>");
+            bw.newLine();
+            bw.write("    <title>");
+            bw.newLine();
+            bw.write("      Nova p&agrave;gina  web");
+            bw.newLine();
+            bw.write("    </title>");
+            bw.newLine();
+            bw.write("  </head>");
+            bw.newLine();
+            bw.write("  <body>");
+            bw.newLine();
+            bw.write("    Nova p&agrave;gina web");
+            bw.newLine();
+            bw.write("  </body>");
+            bw.newLine();
+            bw.write("</html>");
+            bw.newLine();
+            bw.close();
+        }
+    }
+}
+
